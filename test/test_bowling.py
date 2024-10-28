b36139e6c3f5cf33530eb1e1b81393ee78a427e7
t-arr <123.123.123@123.123>
2024-10-28 18:24:43 +0200

    US7 - Test 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -116,3 +116,17 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(8, 1))
         game.add_frame(Frame(2, 6))
         self.assertEqual(112, game.calculate_score())
+
+    def test_calculate_spare_followed_by_spare(self):
+        game = BowlingGame()
+        game.add_frame(Frame(8, 2))
+        game.add_frame(Frame(5, 5))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+        self.assertEqual(98, game.calculate_score())
